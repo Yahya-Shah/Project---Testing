ed6bf26249bf27eab7452ec308e783a0005b8dee
Yahya Shah <33357965+Yahya-Shah@users.noreply.github.com>
2020-07-04 09:29:59 -0700

    Update gitcode-testing.py

--- gitcode-testing.py
+++ gitcode-testing.py
@@ -1,4 +1,9 @@
-a = 3:
-b = 5:
-c = a + b:
+a = 3
+b = 5
+c = a + b
 print ("Your results after addition of two numbers =" c):
+
+d = 5
+e = 2
+f = d - e
+print ("Your results after substraction of two number =" f)
