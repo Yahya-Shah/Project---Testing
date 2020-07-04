3ca3bab9d4a1df8050176b221bfa587981ecc157
Yahya Shah <33357965+Yahya-Shah@users.noreply.github.com>
2020-07-04 10:06:21 -0700

    added a file from computer
    added a file from the computer in the development branch via drag and drop.

---
+++ testing.py
@@ -0,0 +1,14 @@
+#well im not sure how to code in python. Im trying to learn this. I'm also learning github, so this is my first file I will upload on github in development branch#
+
+a = 3
+b = 5
+c = a + b
+
+print ("Hello world", "Your results after addition =" c) 
+
+if ( c < 5):
+	print("its a bigger numer")
+else ():
+	print("It's a smaller numer")
+
+#Now I'm not sure if this code is correct# 
